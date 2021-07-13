786e0d5c02a07fbf95cfa0bed26b42eb97b8245b
Zoan112 <zohar.hadari@gmail.com>
2021-07-13 13:05:14 +0300

    Added location func

--- appp.js
+++ appp.js
@@ -5,18 +5,47 @@ const humidity = document.getElementsByClassName("humidity")[0];
 
 console.log(temp.textContent)
 /* Fetch from API server*/
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-
-  fetch("https://climate-api.zohar-hadari.com:3000/get/livingrooms%20-l", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result) + update(result))
-    .catch(error => console.log('error', error));
 
 
+
+/*Get location selection from DOM*/
+        const locationSelection = ((value)=>{
+            console.log(value);
+            fetchSelectedLocation(value)
+        })
+
+        /*Translation layer*/
+        const fetchSelectedLocation = (value)=>{
+            let locations = { lr: "https://climate-api.zohar-hadari.com:3000/get/livingrooms%20-l", os: "httpsOS"}
+            console.log('fetch', value);
+            console.log(value);
+            console.log(locations.value);
+            console.log('[]',locations[value]);
+
+            const apiCall = locations[value];
+            fetchFuntion(apiCall);
+          //  console.log(Object.values(locations))
+        }
+
+        /*fetch results*/
+
+        fetchFuntion = (apiCall)=>{
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
+              };
+              
+            
+              fetch(apiCall, requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result) + update(result))
+                .catch(error => console.log('error', error));
+            
+
+        }
+      
+
+/*Update DOM*/
     update =((result)=>{
         show = JSON.parse(result);
         result.json
@@ -24,9 +53,3 @@ var requestOptions = {
         humidity.textContent = show[0].H;
         console.log(temp.textContent);
     })
-
-
-
-        const locationSelection = ((value)=>{
-            console.log(value);
-        })
