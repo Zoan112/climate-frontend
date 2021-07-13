ca6b348b4eb8867e6bc197220c06e91ef74aa8d4
Zoan112 <zohar.hadari@gmail.com>
2021-07-13 12:28:26 +0300

    Some styling and selection box added

--- appp.js
+++ appp.js
@@ -3,7 +3,7 @@ console.log("js started");
 const temp = document.getElementsByClassName("temp")[0];
 const humidity = document.getElementsByClassName("humidity")[0];
 
-
+console.log(temp.textContent)
 /* Fetch from API server*/
 var requestOptions = {
     method: 'GET',
@@ -13,17 +13,8 @@ var requestOptions = {
 
   fetch("https://climate-api.zohar-hadari.com:3000/get/livingrooms%20-l", requestOptions)
     .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
-
-
-  fetch("https://climate-api.zohar-hadari.com:3000/get/livingrooms%20-l", requestOptions)
-    .then(response => response.text())
     .then(result => console.log(result) + update(result))
     .catch(error => console.log('error', error));
-
-
 
 
     update =((result)=>{
@@ -31,4 +22,11 @@ var requestOptions = {
         result.json
         temp.textContent = show[0].C;
         humidity.textContent = show[0].H;
+        console.log(temp.textContent);
     })
+
+
+
+        const locationSelection = ((value)=>{
+            console.log(value);
+        })
